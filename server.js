9008dcb28e7be67fc38021534e5e510f0cf3924c
Cj Corp Dev <69812733+cjcorpdev@users.noreply.github.com>
2022-04-05 12:42:33 -0500

    actualizació msn personalizado desde archivo

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 //const
+require('dotenv').config();
 const express = require('express');
 const bodyParser= require('body-parser');
-require('dotenv').config();
+const response=require('./Netword/response')
 const router = express.Router();
 const port = process.env.PORT;
 //server
 var app = express();
 app.use(bodyParser.json());
 app.use(router);
-
 //router
 router.get('/msn', (req, res) => {
-    res.send('Lista de msn');
+    console.log(req.headers);
+    res.header({
+        "custom-header":"nuestro valor personalizado",
+    })
+    response.success(req, res, 'Lista de msn', 200);
 });
 router.post('/msn', (req, res) => {
     console.log(req.query);
-    console.log(req.body);
-    res.send('El msn '+ req.body.text +' a sido enviado.');
+    if (req.query.error == 'ok') {
+        response.error(req, res, 'error simulado', 400)
+    } else {
+        response.success(req, res, 'Creado corectamente', 200);
+    }
 });
 router.get('*', (req, res) => {
-    res.send('Oh escoje bien la opciomn');
+    response.error(req, res, 'error. error. error', 400)
 });
 //listen
 app.listen(port);
